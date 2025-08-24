149ac6f0efca5edbf3987fc19421857635a56081
mikejrdolor <142621281+mikejrdolor@users.noreply.github.com>
2025-08-24 22:30:19 +0800

    Delete newtown directory

--- newtown/button.js
+++
@@ -1,6 +0,0 @@
-// Optional: You can add alert or tracking for button clicks
-document.querySelectorAll(".btn").forEach(button => {
-  button.addEventListener("click", function() {
-    console.log("Navigating to: " + this.textContent);
-  });
-});
